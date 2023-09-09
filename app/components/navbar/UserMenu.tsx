df49aae76ce2cce37f8a6bb4e802f8e55c13975f
Derrick Choong <dchoong90@gmail.com>
2023-09-09 22:50:35 +0800

    add ClientOnly prevent hydration

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,4 +1,5 @@
-'use client'
+'use client';
+
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
@@ -56,44 +57,42 @@ const UserMenu = () => {
             <AiOutlineMenu />
             <div className='hidden md:block'>
               <Avatar />
-
-
             </div>
-
           </div>
         </div>
 
-        {isOpen && (
-          <div
-            className="
-              absolute
-              rounded-xl
-              shadow-md
-              w-[40vw]
-              md:w-3/4
-              bg-white
-              overflow-hidden
-              right-0
-              top-12
-              text-sm
-            "
-          > 
-            <div className="flex flex-col cursor-pointer">
-              <>
-                <MenuItem 
-                  onClick={() => {}}
-                  label='Login'
-                />
-                <MenuItem 
-                  onClick={() => {}}
-                  label='Signup'
-                />
-              </>
+          {isOpen && (
+            <div
+              className="
+                absolute
+                rounded-xl
+                shadow-md
+                w-[40vw]
+                md:w-3/4
+                bg-white
+                overflow-hidden
+                right-0
+                top-12
+                text-sm
+              "
+            > 
+              <div className="flex flex-col cursor-pointer">
+                <>
+                  <MenuItem 
+                    onClick={() => {}}
+                    label='Login'
+                  />
+                  <MenuItem 
+                    onClick={() => {}}
+                    label='Signup'
+                  />
+                </>
 
+              </div>
             </div>
-          </div>
-        )}
+          )}
       </div>
+      
   );
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        
         <Navbar />
+                
         {children}
         </body>
     </html>
