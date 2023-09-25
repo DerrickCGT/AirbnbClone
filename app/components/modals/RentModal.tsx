dfce9789dd07bc2e0ae55a05a19a120fdf2e898b
Derrick Choong <dchoong90@gmail.com>
2023-09-25 13:55:50 +0800

    Update Location and Map: Rent Modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -6,7 +6,9 @@ import Modal from "./Modal"
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
+import CountrySelect from "../inputs/CountrySelect";
 import { FieldValues, useForm } from "react-hook-form";
+import dynamic from "next/dynamic";
 
 enum STEPS {
   CATEGORY = 0,
@@ -48,6 +50,12 @@ const RentModal = () => {
 
   // Using the watch function to get the current value of the 'category' input.
   const category = watch('category');
+  const location = watch('location');
+
+  // dynamically import Map under SSR
+  const Map = useMemo(() => dynamic(() => import('../Map'), {
+    ssr: false
+  }), [location]);
 
   // setValue by default indeed set the value but do not re-rendering the page.
   // Custom function is used to re-rendering the page.
@@ -57,7 +65,7 @@ const RentModal = () => {
       shouldDirty: true,
       shouldTouch: true,
     })
-  }
+  };
 
   const onBack = () => {
     setStep((value) => value - 1);
@@ -115,11 +123,29 @@ const RentModal = () => {
     </div>
   )
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+          title="Where is your place located?"
+          subtitle="Help guests find you!"
+        />
+        <CountrySelect 
+          value={location}
+          onChange={(value) => setCustomValue('location', value)}
+        />
+        <Map 
+          center={location?.latlng}
+        />
+      </div>
+    )
+  }
+
   return (
     <Modal 
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
