063eb8cb275551041e0428e90567b0607a828a9e
Derrick Choong <dchoong90@gmail.com>
2023-09-27 16:20:09 +0800

    fetching listing via server component

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,8 +33,10 @@ export default async function RootLayout({
         <LoginModal />
         <RegisterModal />
         <Navbar currentUser={currentUser} />
-                
-        {children}
+        <div className="pb-20 pt-28">
+          {children}
+        </div>        
+        
         </body>
     </html>
   )
