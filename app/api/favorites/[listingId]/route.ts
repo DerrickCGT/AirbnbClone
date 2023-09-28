6e51eff14eee0fb40e0121bfb0084248518c8b95
Derrick Choong <dchoong90@gmail.com>
2023-09-28 11:59:10 +0800

    Favorite Functionality

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -5,38 +5,70 @@ import prisma from "@/app/libs/prismadb"
 
 
 interface IParams {
-    listingId?: string;
+  listingId?: string;
 }
 
+// Remove default function 
 export async function POST(
-    request: Request,
-    { params }: {params: IParams}
+  request: Request,
+  { params }: {params: IParams}
 ){
-    const currentUser = await getCurrentUser();
+  const currentUser = await getCurrentUser();
 
-    if (!currentUser) {
-        return NextResponse.error();
-    }
-    
-    const { listingId } = params;
+  if (!currentUser) {
+      return NextResponse.error();
+  }
+  
+  const { listingId } = params;
 
-    if (!listingId || typeof listingId !== 'string') {
-        throw new Error('Invalid ID');
-    }
+  if (!listingId || typeof listingId !== 'string') {
+      throw new Error('Invalid ID');
+  }
 
-    let favoriteIds = [...(currentUser.favoriteIds || [])];
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
 
-    favoriteIds.push(listingId);
+  favoriteIds.push(listingId);
 
-    const user = await prisma.user.update({
-        where: {
-            id: currentUser.id
-        },
-        data: {
-            favoriteIds
-        }
-    });
+  const user = await prisma.user.update({
+      where: {
+          id: currentUser.id
+      },
+      data: {
+          favoriteIds
+      }
+  });
 
-    return NextResponse.json(user);
+  return NextResponse.json(user);
 }
 
+export async function DELETE(
+  request: Request,
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+      return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== 'string') {
+      throw new Error('Invalid ID');
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+  const user = await prisma.user.update({
+      where: {
+          id: currentUser.id
+      },
+      data: {
+          favoriteIds
+      }
+  })
+
+  return NextResponse.json(user);
+}
--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorite";
 
 interface HeartButtonProps{
   listingId: string;
@@ -12,11 +13,14 @@ const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser
 }) => {
-  const hasFavourited = false;
-  const toggleFavourite = () => {};
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser
+  });
+  
   return (
     <div
-      onClick={toggleFavourite}
+      onClick={toggleFavorite}
       className="
         relative
         hover:opacity-80
@@ -36,7 +40,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
       <AiFillHeart
         size={24}
         className={
-          hasFavourited? 'fill-rose-500' : 'fill-neutral-500/70'
+          hasFavorited? 'fill-rose-500' : 'fill-neutral-500/70'
         }
       />
     </div>
